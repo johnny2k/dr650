3b0cb6f99fceaf8bbfb841585afba18f21b92639
jonathankohls <jonathankohls@evosolutions.com>
2014-01-18 19:28:11 -0800

    Changes search method to use CL query

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -3,24 +3,42 @@ require 'craigslist'
 
   def index
     if Bike.all.empty? then
-       search_all_nearby_for_bikes
+      search_all_nearby_for_bikes  
     else 
        @bikes = Bike.all
     end   
   end
 
   def search_all_nearby_for_bikes
-    nearby = ["bellingham", "bend", "comoxvalley", "corvallis", "eastoregon", "eugene", "abbotsford", "kelowna", "klamath", "lewiston", "medford", "moseslake", "nanaimo", "olympic", "oregoncoast", "portland", "pullman", "roseburg", "salem", "seattle", "siskiyou", "skagit", "spokane", "sunshine", "kpr", "vancouver", "victoria", "wenatchee", "whistler", "yakima"] 
+    nearby = ["bellingham", "bend", "corvallis", "eastoregon", "eugene", "klamath", "lewiston", "medford", "moseslake", "olympic", "oregoncoast", "portland", "pullman", "roseburg", "salem", "seattle", "siskiyou", "skagit", "spokane", "kpr", "wenatchee", "yakima"] 
     nearby.each do |place|
-      Craigslist.city(place).motorcycles.fetch(1000).each do |bike|
-        found_bike = Bike.new(bike)
-        if !bike["href"].nil? then
-          found_bike["href"] = "#{place}.craigslist.com#{bike["href"]}"
+      if Craigslist.valid_city?(place) then
+        dr_650 = Craigslist.city(place).category(:motorcycles).query("dr 650").fetch(100)
+        dr650 = Craigslist.city(place).category(:motorcycles).query("dr 650").fetch(100)
+        bikes = dr_650 + dr650
+
+        if !bikes.empty? then
+          bikes.each do |bike|
+            found_bike = Bike.new(bike)
+            if /dr(\s|.)?650/i.match(bike["text"]) then
+              puts "Found a dr650"
+              if !/^http/.match(bike["href"])
+                real_url = "http://#{place}.craigslist.com#{bike["href"]}"
+                found_bike.href = real_url
+              end
+              found_bike.save! 
+            end
+          end
         end
-	if /dr(\s|.)?650/i.match(bike[:text]) then 
-	  found_bike.save!
-	end
-      end
+      else
+        raise("#{place} is not a valid city")
+      end      
     end
+  end
+
+  def cities_nearby city
+    page = `curl http://sfbay.craigslist.org/`
+    cities = nil
+    return cities
   end
 end
